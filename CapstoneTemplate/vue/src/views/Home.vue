4187482172f5431834c73a2e8813f80e062bdf5f
Alex Lipinsky <62192604+alex-lip@users.noreply.github.com>
2021-02-05 07:27:32 -0500

    home

--- CapstoneTemplate/vue/src/views/Home.vue
+++ CapstoneTemplate/vue/src/views/Home.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+  <div class="home container-fluid">
+    <div class="row mainRow">
+      <div class="col-md-8">
+        <div class="row formula">
+          <div class="col-md-8">Formula</div>
+          <div class="col-md-3">Result</div>
+        </div>
+        <div class="row formula">
+          <div class="col-md-8">Formula</div>
+          <div class="col-md-3">Result</div>
+        </div>
+        <div class="row formula">
+          <div class="col-md-8">Formula</div>
+          <div class="col-md-3">Result</div>
+        </div>
+        <div class="row formula">
+          <div class="col-md-8">Formula</div>
+          <div class="col-md-3">Result</div>
+        </div>
+      </div>
+      <div class="col-md-3 box">Examples</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
+
+<style scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.container-fluid {
+  background-color: rgb(175, 29, 29);
+}
+
+.row {
+  background-color: white;
+}
+
+.box {
+  border: 1px solid rgb(158, 158, 158);
+  margin: 10px;
+}
+
+.mainRow {
+  margin-top: 50px;
+}
+
+.formula {
+    border: 1px solid rgb(158, 158, 158);
+  margin: 10px;
+}
+</style>
